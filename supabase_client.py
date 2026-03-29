891b4a8ea71bcf29c375e26cb320d9c8782be097
RMBLOGG <ramdanhidayah176@gmail.com>
2026-03-30 04:32:02 +0700

    fix proxy error

--- supabase_client.py
+++ supabase_client.py
@@ -1,5 +1,8 @@
-from supabase import create_client
+from supabase import create_client, Client
+from supabase.lib.client_options import ClientOptions
 from config import Config
 
-supabase = create_client(Config.SUPABASE_URL, Config.SUPABASE_ANON_KEY)
-supabase_admin = create_client(Config.SUPABASE_URL, Config.SUPABASE_SERVICE_KEY) if Config.SUPABASE_SERVICE_KEY else supabase
+options = ClientOptions(auto_refresh_token=False, persist_session=False)
+
+supabase: Client = create_client(Config.SUPABASE_URL, Config.SUPABASE_ANON_KEY, options=options)
+supabase_admin: Client = create_client(Config.SUPABASE_URL, Config.SUPABASE_SERVICE_KEY, options=options) if Config.SUPABASE_SERVICE_KEY else supabase
